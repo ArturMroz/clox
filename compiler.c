54a28c573ebaae81a5d5cfccaa87a63d5cac7879
Artur Mroz <frost47@gmail.com>
2022-11-10 08:53:29 +0000

    add tokens

--- compiler.c
+++ compiler.c
@@ -6,4 +6,17 @@
 
 void compile(const char *source) {
     init_scanner(source);
+    int line = -1;
+    for (;;) {
+        Token token = scan_token();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("   | ");
+        }
+        printf("%2d '%.*s'\n", token.type, token.len, token.start);
+
+        if (token.type == TOKEN_EOF) break;
+    }
 }
--- scanner.c
+++ scanner.c
@@ -17,3 +17,86 @@ void init_scanner(const char *source) {
     scanner.cur = source;
     scanner.line = 1;
 }
+
+static bool is_at_end() {
+    return *scanner.cur == '\0';
+}
+
+static Token make_token(TokenType type) {
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.len = (int)(scanner.cur - scanner.start);
+    token.line = scanner.line;
+
+    return token;
+}
+
+static Token error_token(const char *msg) {
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = msg;
+    token.len = (int)strlen(msg);
+    token.line = scanner.line;
+
+    return token;
+}
+
+static char advance() {
+    scanner.cur++;
+    return scanner.cur[-1];
+}
+
+static bool match(char expected) {
+    if (is_at_end()) return false;
+    if (*scanner.cur != expected) return false;
+
+    scanner.cur++;
+    return true;
+}
+
+Token scan_token() {
+    // since scan_token scans a complete token, we know we are at the beggining
+    // of a new token when we enter the function
+    scanner.start = scanner.cur;
+
+    if (is_at_end()) return make_token(TOKEN_EOF);
+
+    char c = advance();
+
+    switch (c) {
+    case '(':
+        return make_token(TOKEN_LEFT_PAREN);
+    case ')':
+        return make_token(TOKEN_RIGHT_PAREN);
+    case '{':
+        return make_token(TOKEN_LEFT_BRACE);
+    case '}':
+        return make_token(TOKEN_RIGHT_BRACE);
+    case ';':
+        return make_token(TOKEN_SEMICOLON);
+    case ',':
+        return make_token(TOKEN_COMMA);
+    case '.':
+        return make_token(TOKEN_DOT);
+    case '-':
+        return make_token(TOKEN_MINUS);
+    case '+':
+        return make_token(TOKEN_PLUS);
+    case '/':
+        return make_token(TOKEN_SLASH);
+    case '*':
+        return make_token(TOKEN_STAR);
+
+    case '!':
+        return make_token(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+    case '=':
+        return make_token(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+    case '<':
+        return make_token(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+    case '>':
+        return make_token(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+    }
+
+    return error_token("Unexpected character.");
+}
