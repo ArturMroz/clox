ebaf93c64f1ad083ef895de23126abf283812b40
Artur Mroz <frost47@gmail.com>
2022-11-13 17:26:09 +0000

    parse prefix expressions

--- compiler.c
+++ compiler.c
@@ -13,7 +13,28 @@ typedef struct {
     bool panic_mode;
 } Parser;
 
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT, // =
+    PREC_OR,         // or
+    PREC_AND,        // and
+    PREC_EQUALITY,   // == !=
+    PREC_COMPARISON, // < > <= >=
+    PREC_TERM,       // + -
+    PREC_FACTOR,     // * /
+    PREC_UNARY,      // ! -
+    PREC_CALL,       // . ()
+    PREC_PRIMARY
+} Precedence;
+
 Parser parser;
+Chunk *compiling_chunk;
+
+static Chunk *current_chunk() {
+    return compiling_chunk;
+}
+
+// ERROR REPORTING
 
 static void error_at(Token *tok, const char *msg) {
     if (parser.panic_mode) return;
@@ -41,6 +62,8 @@ static void error_at_current(const char *msg) {
     error_at(&parser.cur, msg);
 }
 
+// ADVANCING
+
 static void advance() {
     parser.prev = parser.cur;
 
@@ -61,8 +84,80 @@ static void consume(TokenType type, const char *msg) {
     error_at_current(msg);
 }
 
+// EMITTING BYTES
+
+static void emit_byte(uint8_t byte) {
+    write_chunk(current_chunk(), byte, parser.prev.line);
+}
+
+static void emit_bytes(uint8_t byte1, uint8_t byte2) {
+    emit_byte(byte1);
+    emit_byte(byte2);
+}
+
+static void emit_return() {
+    emit_byte(OP_RETURN);
+}
+
+static void end_compiler() {
+    emit_return();
+}
+
+static uint8_t make_constant(Value val) {
+    int constant = add_constant(current_chunk(), val);
+    if (constant > UINT8_MAX) {
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+
+    return (uint8_t)constant;
+}
+
+static void emit_constant(Value val) {
+    emit_bytes(OP_CONSTANT, make_constant(val));
+}
+
+// EXPRESSIONS
+
+static void expression() {
+    parse_precedence(PREC_ASSIGNMENT);
+}
+
+static void grouping() {
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void number() {
+    double val = strtod(parser.prev.start, NULL);
+    emit_constant(val);
+}
+
+static void unary() {
+    TokenType operator_type = parser.prev.type;
+
+    // compile the operand
+    parse_precedence(PREC_ASSIGNMENT);
+
+    // emit operator instruction
+    switch (operator_type) {
+    case TOKEN_MINUS:
+        emit_bytecode(OP_NEGATE);
+        break;
+
+    default:
+        return; // unreachable
+    }
+}
+
+static void parse_precedence(Precedence precedence) {
+}
+
+// MAIN
+
 bool compile(const char *source, Chunk *chunk) {
     init_scanner(source);
+    compiling_chunk = chunk;
 
     parser.had_error = false;
     parser.panic_mode = false;
@@ -70,5 +165,6 @@ bool compile(const char *source, Chunk *chunk) {
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
+    end_compiler();
     return !parser.had_error;
 }
